fa00ba85959762d6a00f4cc3f5ebca36a3d0d1af
hkm <rbals0445@naver.com>
2022-01-28 15:13:33 +0900

    release에서 master -> main

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -3,6 +3,6 @@ const myfs = require('fs');
 const originPath = './raw/';
 
 myfs.readdirSync(originPath).forEach(file => {
-    let updatedFileName = file.replace('/=/g','_');
+    let updatedFileName = file.replace('=','_');
     myfs.renameSync(originPath+file,originPath+updatedFileName);
 })
