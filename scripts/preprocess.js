4ee741a729b3e3486e716d8c4b450857cdbfb797
hkm <rbals0445@naver.com>
2022-01-28 14:52:51 +0900

    파일명수정, node 12버전기준이라 그런가.. 아니면 노드자체가 replaceAll 이 안먹히나..

--- scripts/preprocess.js
+++ scripts/preprocess.js
@@ -3,6 +3,6 @@ const myfs = require('fs');
 const originPath = './raw/';
 
 myfs.readdirSync(originPath).forEach(file => {
-    let updatedFileName = file.replaceAll('=','_');
+    let updatedFileName = file.replace('/=/g','_');
     myfs.renameSync(originPath+file,originPath+updatedFileName);
 })
